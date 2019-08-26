63de91ed9c51537348feae7c52e6f741b4a1ada7
franz-osvaldo <franz.osvaldo.beltran@gmail.com>
2019-08-26 00:26:59 -0400

    Agregar nuevos metodos

--- Programa.cpp
+++ Programa.cpp
@@ -1,29 +1,38 @@
-#include<iostream> // std::cout(objeto), std::-endl(manipulador de flujo)
+#include<iostream> // std::cout(objeto), std::endl(manipulador de flujo)
 #include<iomanip>  // std::setw(), std::setfill()
 int main()
 {   
+    std::cout << "\n----------------------------------------------------------------------------PROGRAMA MAS ORGINAL DEL MUNDO\n";
     std::cout<< "HOLA MUNDO";
-    std::cout.put('W') << std::endl;
-    std::cout.put(87);
-    std::cout << 'W'<< std::endl << "HOLA"<<std::endl;
     
-    std::cout << "-----------------------------------------------------------NUEVA LINEA" << std::endl ;
+    std::cout << "\n-----------------------------------------------------------------------------------MOSTRAR NUMEROS ENTEROS\n";
+    std::cout << 100;
+    
+    std::cout << "\n------------------------------------------------------------------------------------MOSTRAR NUMEROS REALES\n";
+    std::cout << 10.5; 
+    
+    std::cout << "\n-------------------------------------------------------------------------------------------MOSTRAR CADENAS\n";
+    std::cout << "Se amable con los nerds: es muy probable que termines trabajando para uno de ellos (Bill Gates)";
+    
+    std::cout << "\n---------------------------------------------------------------------------------------------VARIAS LINEAS\n";
     std::cout << "Quien ama la practica sin teoria" << std::endl;
     std::cout << "es como el marinero que se embarca sin timon ni brujula" << std::endl;
     std::cout << "y nunca sabe donde ir." << std::endl;
     std::cout << "-Leornardo da Vinci" << std::endl;
+    
+    std::cout << "\n---------------------------------------------------------------------------------------------CONCATENACION\n";
+    std::cout << "Si tengo " << 10 << " manzanas, y me como "<< 8 << ", entonces me quedan " << 10-8 << std::endl;  
 
-    std::cout << "---------------------------------------------------FORMATEAR LA SALIDA" << std::endl ;
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Estamos construyendo una agenda telefonica                                                     //
-    //                                                                                                // 
-    //                               NOMBRE      TELEFONO                                             //  
-    //                        Franz Beltran     759368942                                             //  
-    //                          Juan Peredo     758524556                                             // 
-    //                            Ana Ortiz     759235585                                             //
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    std::cout << "ANCHO DE CAMPO:" << std::endl;
-    std::cout << "El ancho del campo actual es: " << std::cout.width() << std::endl;
+    std::cout << "\n---------------------------------------------------------------------------------------FORMATEAR LA SALIDA\n";
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Estamos construyendo una agenda telefonica                                                                               //
+    //                                                                                                                          // 
+    //                                                  NOMBRE      TELEFONO                                                    //  
+    //                                           Franz Beltran     759368942                                                    //  
+    //                                             Juan Peredo     758524556                                                    // 
+    //                                               Ana Ortiz     759235585                                                    //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "ANCHO DE CAMPO ACTUAL ES :" << std::cout.width() << std::endl;
     std::cout.width(18);    
     std::cout << "NOMBRE";
     std::cout.width(13);    
@@ -44,38 +53,80 @@ int main()
     // std::cout << "Jhon";
     // std::cout.width(13);    
     // std::cout << "80257777785264260111" << std::endl;
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Estamos construyendo una nomina de pagos:                                                      //
-    //                                                                                                // 
-    //                               NOMBRE       MONTO                                               //  
-    //                        Franz Beltran: $*****8942                                               //  
-    //                          Juan Peredo: $***524556                                               // 
-    //                            Ana Ortiz: $*59235585                                               //
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    std::cout << "CARACTER DE RELLENO:" << std::endl;
-    std::cout.width(9);
-    std::cout << "Franz: $";
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Estamos construyendo una nomina de pagos:                                                                                //
+    //                                                                                                                          // 
+    //                                                    NOMBRE    MONTO                                                       //  
+    //                                          Franz Beltran: $*****8942                                                       //  
+    //                                            Juan Peredo: $***524556                                                       // 
+    //                                              Ana Ortiz: $*59235585                                                       //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "CARACTER DE RELLENO ACTUAL ES:" << std::cout.fill() << std::endl;
+    std::cout.width(18);
+    std::cout << "NOMBRE";
+    std::cout.width(13);
+    std::cout << "MONTO" << std::endl;
+    std::cout.width(18);
+    std::cout << "Franz Beltran: $";
+    std::cout.width(13);
     std::cout.fill('*');
-    std::cout.width(8);
-    std::cout << 1500 << std::endl;
-
-    std::cout.width(9);
-    std::cout << "Carlos: $";
+    std::cout << 8942 << std::endl;
+    std::cout.width(18);
+    std::cout.fill(' ');
+    std::cout << "Juan Peredo: $";
     std::cout.fill('*');
-    std::cout.width(8);
-    std::cout << 37500 << std::endl;
-
+    std::cout.width(13);
+    std::cout << 524556 << std::endl;
+    std::cout.width(18);
+    std::cout.fill(' ');
+    std::cout << "Ana Ortiz: $";
+    std::cout.fill('*');
+    std::cout.width(13);
+    std::cout << 59235585 << std::endl;
     std::cout.fill(' ');
 
-    std::cout << "---------------------------------------------MANIPULADORES ADICIONALES" << std::endl ;
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Estamos construyendo una agenda telefonica                                                     //
-    //                                                                                                // 
-    //                               NOMBRE      TELEFONO                                             //  
-    //                        Franz Beltran     759368942                                             //  
-    //                          Juan Peredo     758524556                                             // 
-    //                            Ana Ortiz     759235585                                             //
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    std::cout << "\n-------------------------------------------------------------VALORES BOOL COMO TRUE O FALSE-FUNCION setf()\n";
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Deseamos mostrar el resultado de evaluar expresiones logicas:                                                            //
+    // El resultado de evaluar la expresion ( 3 > 2 ) es: true                                                                  // 
+    // El resultado de evaluar la expresion ( 3 < 2 ) es: false                                                                 // 
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << ( 3 > 2 ) << std::endl;
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << true << std::endl;
+    std::cout << "El resultado de evaluar ( 3 < 2 ) es: " << ( 3 < 2  ) << std::endl;
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << false << std::endl;
+    
+
+    std::cout.setf(std::ios_base::boolalpha);
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << ( 3 > 2 ) << std::endl;
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << true << std::endl;
+    std::cout << "El resultado de evaluar ( 3 < 2 ) es: " << ( 3 < 2  ) << std::endl;
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << false << std::endl;
+
+    std::cout.unsetf(std::ios_base::boolalpha);
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << ( 3 > 2 ) << std::endl;
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << true << std::endl;
+    std::cout << "El resultado de evaluar ( 3 < 2 ) es: " << ( 3 < 2  ) << std::endl;
+    std::cout << "El resultado de evaluar ( 3 > 2 ) es: " << false << std::endl;
+    
+    // std::cout << "RETORNO: " << std::cout.setf(std::ios_base::boolalpha) << std::endl;
+    // std::cout.unsetf(std::ios_base::boolalpha);
+    // std::cout << "mostrar true o false"<< std::endl ; 
+    // std::cout << true << std::endl;
+    // std::cout << (3 == 2 ) << std::endl;
+    // std::cout << "RETORNO: " << std::cout.setf(std::ios_base::boolalpha) << std::endl;
+    std::cout << "\n---------------------------------------------------------------------------------MANIPULADORES ADICIONALES\n";
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Estamos construyendo una agenda telefonica                                                                               //
+    //                                                                                                                          // 
+    //                                                  NOMBRE      TELEFONO                                                    //  
+    //                                           Franz Beltran     759368942                                                    //  
+    //                                             Juan Peredo     758524556                                                    // 
+    //                                               Ana Ortiz     759235585                                                    //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     std::cout << "ANCHO DE CAMPO:" << std::endl;
 
     std::cout<< std::setw(18) <<"NOMBRE" << std::setw(13) << "TELEFONO" << std::endl;
@@ -84,14 +135,14 @@ int main()
     std::cout<< std::setw(18) <<"Ana Ortiz" << std::setw(13) << "68539368" << std::endl;
      // solo afecta al elemento siguiente
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Estamos construyendo una nomina de pagos:                                                      //
-    //                                                                                                // 
-    //                               NOMBRE       MONTO                                               //  
-    //                        Franz Beltran: $*****8942                                               //  
-    //                          Juan Peredo: $***524556                                               // 
-    //                            Ana Ortiz: $*59235585                                               //
-    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Estamos construyendo una nomina de pagos:                                                                                //
+    //                                                                                                                          // 
+    //                                                    NOMBRE    MONTO                                                       //  
+    //                                          Franz Beltran: $*****8942                                                       //  
+    //                                            Juan Peredo: $***524556                                                       // 
+    //                                              Ana Ortiz: $*59235585                                                       //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     std::cout << "CARACTER DE RELLENO:" << std::endl;
 
     std::cout<< std::setw(18) <<"NOMBRE" << std::setw(13) << "TELEFONO" << std::endl;
